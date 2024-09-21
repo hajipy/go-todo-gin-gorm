4fee82a9e0f0effeda7e6832cd2dd3a1e8df35b7
Kenichi Saito <hajime@studiohff.net>
2024-09-21 19:50:29 +0900

    Standardized environment variable names for database config

--- backend/main.go
+++ backend/main.go
@@ -13,11 +13,11 @@ import (
 )
 
 type Config struct {
-	DBHost     string `env:"TODO_DB_HOST" envDefault:"127.0.0.1"`
-	DBPort     int    `env:"TODO_DB_PORT" envDefault:"33306"`
-	DBUser     string `env:"TODO_DB_USER" envDefault:"todo"`
-	DBPassword string `env:"TODO_DB_PASSWORD" envDefault:"todo"`
-	DBName     string `env:"TODO_DB_NAME" envDefault:"todo"`
+	DBHost     string `env:"DB_HOST" envDefault:"127.0.0.1"`
+	DBPort     int    `env:"DB_PORT" envDefault:"33306"`
+	DBUser     string `env:"DB_USER" envDefault:"todo"`
+	DBPassword string `env:"DB_PASSWORD" envDefault:"todo"`
+	DBName     string `env:"DB_NAME" envDefault:"todo"`
 }
 
 type Todo struct {
