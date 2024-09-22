b033a3d3907d9641f97ccfab11a2f8dc3a7178bf
Kenichi Saito <hajime@studiohff.net>
2024-09-22 22:56:54 +0900

    Added functionality for adding, updating, and deleting TODO items

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,16 +17,77 @@ function App() {
       .then((data) => setTodos(data));
   }, []);
 
+  const [newTodoTitle, setNewTodoTitle] = useState("");
+
   return (
     <>
       <h1>TODO</h1>
-      <ul>
+      <div>
         {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.id} {todo.title} {todo.completed_at}
-          </li>
+          <div key={todo.id}>
+            <input
+              type="checkbox"
+              name="is_completed"
+              checked={todo.completed_at !== null}
+              onChange={(e) => {
+                fetch(`${backendUrl}/todo/${todo.id}`, {
+                  method: "PATCH",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({ is_completed: e.target.checked }),
+                })
+                  .then((response) => response.json())
+                  .then((updateTodo: Todo) =>
+                    setTodos(
+                      todos.map((todo) =>
+                        todo.id === updateTodo.id ? updateTodo : todo,
+                      ),
+                    ),
+                  );
+              }}
+            />
+            <span>{todo.title}</span>
+            &nbsp;
+            <span>{todo.completed_at}</span>
+            <button
+              onClick={() => {
+                fetch(`${backendUrl}/todo/${todo.id}`, {
+                  method: "DELETE",
+                }).then(() => setTodos(todos.filter((t) => t.id !== todo.id)));
+              }}
+            >
+              Delete
+            </button>
+          </div>
         ))}
-      </ul>
+      </div>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetch(`${backendUrl}/todo`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ title: newTodoTitle }),
+          })
+            .then((response) => response.json())
+            .then((newTodo: Todo) => {
+              setTodos([...todos, newTodo]);
+              setNewTodoTitle("");
+            });
+        }}
+      >
+        <input
+          type="text"
+          name="title"
+          value={newTodoTitle}
+          onChange={(e) => setNewTodoTitle(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
     </>
   );
 }
