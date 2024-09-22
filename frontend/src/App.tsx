f07ec32deede2b989906bd7a85973af1bc590d24
Kenichi Saito <hajime@studiohff.net>
2024-09-22 22:18:36 +0900

    Clean up Vite boilerplate files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,5 @@
 import { useEffect, useState } from "react";
 
-import viteLogo from "/vite.svg";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 type Todo = {
@@ -13,7 +9,6 @@ type Todo = {
 };
 
 function App() {
-  const [count, setCount] = useState(0);
   const [todos, setTodos] = useState<Todo[]>([]);
 
   useEffect(() => {
@@ -24,36 +19,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>
-        <h2>TODO</h2>
-        <ul>
-          {todos.map((todo) => (
-            <li key={todo.id}>
-              {todo.id} {todo.title} {todo.completed_at}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <h1>TODO</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.id} {todo.title} {todo.completed_at}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
